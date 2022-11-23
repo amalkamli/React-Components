e332d34a8558bc7530344559546c5da4dae77efe
core.pager=cat <amal.kemly@gmail.com>
2022-11-23 21:15:31 +0100

    first commit

---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FullName = () => {
+  return (
+    <main>
+        <h1>Amal Kemly</h1>
+    </main>
+  )
+}
+
+export default FullName
---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import phto from './imgs/phto.png';
+
+
+const ProfilePhoto = () => {
+
+  return (
+    <div>
+      <img src={phto} alt=""width="300" class="rounded-circle"/>
+    </div>
+  )
+}
+
+export default ProfilePhoto
